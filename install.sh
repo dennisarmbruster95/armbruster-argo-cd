58c087616038e7caf850bf238eb6710474dbbf20
Dennis Armbruster <Dennis.Armbruster@live.de>
2023-11-26 22:01:48 +0100

    update

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 kubectl create ns monitoring
 kubectl create ns nextcloud
 kubectl create ns ingress
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/mandatory.yaml
 minikube addons enable ingress
 helm repo add argo-cd https://argoproj.github.io/argo-helm
 helm repo add kong https://charts.konghq.com
