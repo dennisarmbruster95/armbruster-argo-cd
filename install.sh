f9a547cf0c4cb1b4176c82b2d402d40e71453037
Dennis Armbruster <Dennis.Armbruster@live.de>
2023-11-26 18:23:14 +0100

    add kong repo

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@ kubectl create ns monitoring
 kubectl create ns nextcloud
 kubectl create ns ingress
 helm repo add argo-cd https://argoproj.github.io/argo-helm
+helm repo add kong https://charts.konghq.com
 helm dep update charts/argo-cd/
 helm install argo-cd charts/argo-cd/
 helm template charts/armbruster-server/ | kubectl apply -f -
