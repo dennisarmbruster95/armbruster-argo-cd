2dbe8e22309ba8b9c719b612eb2cdc8636ac4c8d
Dennis Armbruster <Dennis.Armbruster@live.de>
2023-11-26 18:59:12 +0100

    install emberstack manual

--- install.sh
+++ install.sh
@@ -3,6 +3,9 @@ kubectl create ns nextcloud
 kubectl create ns ingress
 helm repo add argo-cd https://argoproj.github.io/argo-helm
 helm repo add kong https://charts.konghq.com
+helm repo add emberstack https://emberstack.github.io/helm-charts
+helm repo update
+helm upgrade --install reflector emberstack/reflector
 helm dep update charts/argo-cd/
 helm install argo-cd charts/argo-cd/
 helm template charts/armbruster-server/ | kubectl apply -f -
