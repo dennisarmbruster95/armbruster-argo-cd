04308b8f9cd8dbf75575d33a9ae8ae8fd9f29e56
Dennis Armbruster <Dennis.Armbruster@live.de>
2023-11-26 10:44:47 +0100

    Add create ns

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
+kubectl create ns monitoring
+kubectl create ns nextcloud
+kubectl create ns ingress
 helm repo add argo-cd https://argoproj.github.io/argo-helm
 helm dep update charts/argo-cd/
 helm install argo-cd charts/argo-cd/
