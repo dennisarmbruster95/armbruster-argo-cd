084fe0a25c7a6dfaed4684e33449010925c6bca2
Dennis Armbruster <Dennis.Armbruster@live.de>
2023-11-26 10:36:31 +0100

    better install

--- install.sh
+++ install.sh
@@ -1,2 +1,4 @@
+helm repo add argo-cd https://argoproj.github.io/argo-helm
+helm dep update charts/argo-cd/
 helm install argo-cd charts/argo-cd/
 helm template charts/armbruster-server/ | kubectl apply -f -
