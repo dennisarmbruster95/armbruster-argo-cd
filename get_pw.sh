d23241164a7cc514e88e06185d451ac72dc0df7b
Dennis Armbruster <Dennis.Armbruster@live.de>
2024-01-28 20:53:31 +0100

    Update namespace

--- get_pw.sh
+++ get_pw.sh
@@ -1,1 +1,1 @@
-kubectl get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
+kubectl get secret -n argo-cd argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
